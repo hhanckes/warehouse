29e9823fdc02b142c4091ccc50899b04a740d533
Hans <hhanckes@gmail.com>
2014-10-06 20:10:20 -0500

    cap config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+#role :app, %w{deploy@example.com}
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
 
 
 # Extended Server Syntax
@@ -15,7 +15,9 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+set :application, 'warehouse'
+
+server 'ec2-54-88-40-187.compute-1.amazonaws.com', user: 'ubuntu', roles: %w{web app}
 
 
 # Custom SSH Options
