a0b7fd244e648120b14b5c9ac99ba43a3ad97457
Hans <hhanckes@gmail.com>
2014-10-18 16:18:04 -0500

    javascript update price to pay

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -101,3 +101,25 @@ function update_collection_day() {
 		}
 	});
 }
+
+function update_total_to_pay(checkbox, to_pay) {
+	total = parseInt($('.total_to_pay').html().replace(/[^0-9]/g,''));
+	if(checkbox.checked) {
+		total = total + to_pay
+	} else {
+		total = total - to_pay		
+	}
+	$('.total_to_pay').html('$ '+addCommas(total));
+}
+
+function addCommas(nStr) {
+	nStr += '';
+	x = nStr.split(',');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + '.' + '$2');
+	}
+	return x1 + x2;
+}
