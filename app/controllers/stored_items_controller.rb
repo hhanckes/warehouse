14324ce2451a3c6e9dd466933fbbef993956543e
Hans <hhanckes@gmail.com>
2014-10-19 21:39:02 -0500

    bug fix

--- app/controllers/stored_items_controller.rb
+++ app/controllers/stored_items_controller.rb
@@ -7,15 +7,16 @@ class StoredItemsController < ApplicationController
     add_breadcrumb "Inventario", stored_items_path
     
     @in_warehouse = OrderStorageItemStatus.find_by_name('In warehouse')
+    @waiting_funds = OrderStorageItemStatus.find_by_name('Wating funds confirmation for return')
     @return_in_progress = OrderStorageItemStatus.find_by_name('Return in progress')
     
     
     unless params[:q].blank?
       @q = params[:q]
-      @stored_items = current_user.stored_items.joins(:order_storage_item).where('order_storage_items.order_storage_item_status_id = ? or order_storage_items.order_storage_item_status_id = ?', @in_warehouse.id, @return_in_progress.id).where('stored_items.order_storage_item_id = "'+@q+'" or stored_items.name like "%'+@q+'%" or stored_items.description like "%'+@q+'%"').uniq
+      @stored_items = current_user.stored_items.joins(:order_storage_item).where('order_storage_items.order_storage_item_status_id in (?)', [@in_warehouse.id, @return_in_progress.id, @waiting_funds.id]).where('stored_items.order_storage_item_id = "'+@q+'" or stored_items.name like "%'+@q+'%" or stored_items.description like "%'+@q+'%"').uniq
       @in_box = true if is_number? @q
     else
-      @stored_items = current_user.stored_items.joins(:order_storage_item).where('order_storage_items.order_storage_item_status_id = ? or order_storage_items.order_storage_item_status_id = ?', @in_warehouse.id, @return_in_progress.id)
+      @stored_items = current_user.stored_items.joins(:order_storage_item).where('order_storage_items.order_storage_item_status_id in (?)', [@in_warehouse.id, @return_in_progress.id, @waiting_funds.id])
     end
   end
   
